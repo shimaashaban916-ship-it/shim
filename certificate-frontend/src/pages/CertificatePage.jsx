e6cb9f8217e7b6820531976f848cad56f9a37780
Hamed Nour El-Din <hamednoureldein0@gmail.com>
2026-02-06 03:20:43 +0200

    33

--- certificate-frontend/src/pages/CertificatePage.jsx
+++ certificate-frontend/src/pages/CertificatePage.jsx
@@ -92,8 +92,8 @@ const CertificatePage = () => {
 
     if (!qrInstanceRef.current) {
       qrInstanceRef.current = new QRCodeStyling({
-        width: 140,
-        height: 140,
+        width: 145,
+        height: 145,
         type: "svg",
         data: dataUrl,
         dotsOptions: { type: "square", color: "#040404ff" },
